d49c4a79e483b0cfc5810c02c45473bebc5d34a5
WendyZhang05 <magicling0705@gmail.com>
2018-08-28 22:01:48 -0400

    keep working on gmodel_final.py

--- gmodel_final.py
+++ gmodel_final.py
@@ -54,6 +54,7 @@ def fixsender(sender, allsenders=None):
         dns=".".join(pieces[-2:])
     else:
         dns=".".join(pieces[-3:])
+    # get dns in dns dictionary mapping, if not get, return dns
     dns=dnsmapping.get(dns,dns)
     return mpieces[0]+"@"+dns
 
@@ -99,4 +100,120 @@ def parsemaildata(md):
     return iso+tz
 
 def parseheader(hdr,allsenders=None):
-    
+    if hdr is None or len(hdr)<1: return None
+    sender=None
+    x=re.findall('\nFrom: .* <(\S+@\S+)>\n', hdr)
+    if len(x)>=1:
+        sender=x[0]
+    else:
+        x=re.findall('\nFrom: (\S+@\S+)\n', hdr)
+        if len(x)>=1:
+            sender=x[0]
+
+    sender=fixsender(sender,allsenders)
+
+    date=None
+    y=re.findall('\nDate: .*, (.*)\n', hdr)
+    sent_at=None
+    if len(y)>=1:
+        tdate=y[0]
+        tdate=tdate[:26]
+        try:
+            sent_at=parsemaildate(tdate)
+        except: Exception as e:
+            return None
+
+    subject=None
+    z=re.findall('\nSubject: (.*)\n', hdr)
+    if len(z)>=1: subject=z[0].strip().lower()
+
+    guid=None
+    z=re.findall('\nMessage-ID: (.*)\n', hdr)
+    if len(z)>=1: guid=z[0].strip().lower()
+
+    if sender is None or sent_at is None or subject is None or guid is None:
+        return None
+    return (guid, sender, subject, sent_at)
+
+conn=sqlite3.connect('index.sqlite')
+cur=conn.cursor()
+
+cur.execute('DROP TABLE IS EXISTS Messges')
+cur.execute('DROP TABLE IS EXISTS Senders')
+cur.execute('DROP TABLE IS EXISTS Subjects')
+cur.execute('DROP TABLE IS EXISTS Replies')
+
+# Blob data is a field that holds large amounts of data per record.
+cur.execute('''CREATE TABLE IS NOT EXISTS Messages
+    (id INTEGER PRIMARY KEY, guid TEXT UNIQUE, sent_at INTEGER,
+     sender_id INTEGER, subject_id INTEGER,headers BLOB, body BLOB)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Senders
+    (id INTEGER PRIMARY KEY, sender TEXT UNIQUE)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Subjects
+    (id INTEGER PRIMARY KEY, subject TEXT UNIQUE)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Replies
+    (from_id INTEGER, to_id INTEGER)''')
+
+conn_1=sqlite3.connect('mapping.sqlite')
+cur_1=conn_1.cursor()
+
+cur_1.execute('SELECT old, new FROM DNSMapping')
+for message_row in cur_1:
+    dnsmapping[message_row[0].strip().lower()]=message_row[1].strip().lower()
+
+mapping=dict()
+cur_1.execute('SELECT old, new FROM Mapping')
+for message_row in cur_1:
+    old=fixsender(message_row[0])
+    new=fixsender(message_row[1])
+    mapping[old]=fixsender(new)
+
+conn_1.close()
+
+#open main content database in read only mode
+conn_1=sqlite3.connect('file:rawemail.sqlite?mode=ro', uri=True)
+cur_1=conn_1.cursor()
+
+allsenders=list()
+cur_1.execute('SELECT email FROM Messages')
+for message_row in cur_1:
+    sender=fixsender(message[0])
+    if sender is None: continue
+    if 'gmane.org' in sender: continue
+    if sender in allsenders: continue
+    allsenders.append(sender)
+
+print("loaded all senders", len(allsenders), "and mapping", len(mapping), "and dnsmapping", len(dnsmapping))
+
+cur_1.execute('''SELECT header, body, sent_at FROM Messages
+    ORDER BY sent_at''')
+
+senders=dict()
+subjects=dict()
+guids=dict()
+
+count=0
+
+for message_row in cur_1:
+    hdr=message_row[0]
+    parsed=parseheader(hdr, allsender)
+    if parsed is None: continue
+    (guid,sender,subject,sent_at)=parsed
+
+    sender=mapping.get(sender, sender)
+
+    count=count+1
+    if count%250==1: print(count, sent_at, sender)
+
+    if 'gmane.org' in sender:
+        print("error in sender===", sender)
+
+    sender_id=senders.get(sender, None)
+    subject_id=subjects.get(sender, None)
+    guid_id=guids.get(sender, None)
+
+    if sender_id is None:
+        cur.execute('INSERT OR IGNORE INTO Senders(sender) VALUES(?)', (sender,))
+        conn.commit()
+        cur.execute('SELECT FROM Senders WHERE sender=? LIMIT 1', (sender, ))
+        
